1cf83bc1e3c42acbd2b7fe9eb534e930b543cd81
Roulianosss <julien.ortscheid@gmail.com>
2022-11-16 11:20:28 +0100

    test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const apiKey = process.env.REACT_APP_API_KEY
 
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`).then(res => res.json()).then(data => res.json({movies: data.results}))
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`).then(res => res.json()).then(data => {res.json({movies: data.results}); console.log(data)})
 })
 
 module.exports = router;
