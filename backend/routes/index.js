dcf88c52de5479d48fc284cbc3543397a9eb1d8d
Roulianosss <julien.ortscheid@gmail.com>
2022-11-16 11:01:58 +0100

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch')
+require('dotenv').config()
+
+const apiKey = process.env.REACT_APP_API_KEY
+
+router.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`).then(res => res.json()).then(data => res.json({movies: data.results}))
+})
+
+module.exports = router;
---
+++ frontend/components/Home.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import { Popover, Button } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+
+import Movie from './Movie';
+import 'antd/dist/antd.css';
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  const [likedMovies, setLikedMovies] = useState([]);
+  const [movies, setMovies] = useState([])
+
+  // Liked movies (inverse data flow)
+  const updateLikedMovies = (movieTitle) => {
+    if (likedMovies.find(movie => movie === movieTitle)) {
+      setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
+    } else {
+      setLikedMovies([...likedMovies, movieTitle]);
+    }
+  };
+
+  const likedMoviesPopover = likedMovies.map((data, i) => {
+    return (
+      <div key={i} className={styles.likedMoviesContainer}>
+        <span className="likedMovie">{data}</span>
+        <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(data)} className={styles.crossIcon} />
+      </div>
+    );
+  });
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>
+      {likedMoviesPopover}
+    </div>
+  );
+
+  // Movies list
+  // const moviesData = [];
+
+  useEffect(()=>{
+    console.log('effect')
+    fetch('http://localhost:3000/movies').then(res => res.json()).then(data => {
+      setMovies( data.movies.map((data, i) => {
+        const isLiked = likedMovies.some(movie => movie === data.title);
+        return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={data.overview} poster={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${data.poster_path}`} voteAverage={data.vote_average} voteCount={data.vote_count} />;
+      }))
+    })
+  }, [])
+
+  console.log('render')
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.logocontainer}>
+          <img src="logo.png" alt="Logo" />
+          <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
+        </div>
+        <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
+          <Button>♥ {likedMovies.length} movie(s)</Button>
+        </Popover>
+      </div>
+      <div className={styles.title}>LAST RELEASES</div>
+      <div className={styles.moviesContainer}>
+        {movies}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
